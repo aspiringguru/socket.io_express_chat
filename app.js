8aa4f3d49619f4ace3a0136d182db5bb9448bad5
aspiringguru <bmatthewtaylor@gmail.com>
2018-11-29 13:54:15 +1000

    not working, feedback.html error, pauseing work here

--- app.js
+++ app.js
@@ -24,4 +24,24 @@ const io = require("socket.io")(server)
 //listen on every connection
 io.on('connection', (socket) => {
 	console.log('New user connected')
+
+  //default username
+	socket.username = "Anonymous"
+
+  //listen on change_username
+  socket.on('change_username', (data) => {
+      //nb: this console.log appears in server command line bash shell or log files.
+      console.log("change_username event, data.username = ", data.username);
+      socket.username = data.username;
+  })
+
+  //listen on new_message
+  socket.on('new_message', (data) => {
+    //broadcast the new message
+    console.log("new_message event : data.message = ", data.message)
+    console.log("socket.username = ", socket.username)
+    io.sockets.emit('new_message', {message : data.message, username : socket.username});
+  })
+
+
 })
--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,35 @@
 $(function(){
    	//make connection
 	var socket = io.connect('http://localhost:3000')
+
+  //buttons and inputs
+	var message = $("#message")
+	var username = $("#username")
+	var send_message = $("#send_message")
+	var send_username = $("#send_username")
+	var chatroom = $("#chatroom")
+
+  //Emit message
+	send_message.click(function(){
+    console.log("send_message.click(function() ...")
+    console.log("message.val() = ", message.val())
+		socket.emit('new_message', {message : message.val()})
+	})
+
+	//Listen on new_message
+	socket.on("new_message", (data) => {
+		feedback.html('');
+		message.val('');
+		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+	})
+
+  //Emit a username
+	send_username.click(function(){
+    //nb: this console.log appears in browswer console.
+    console.log("send_username.click(function()...")
+    console.log("username.val() = ", username.val())
+		socket.emit('change_username', {username : username.val()})
+	})
+
+
 });
